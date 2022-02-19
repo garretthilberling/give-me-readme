4d20ca39333923350a1e24b272714a6a44afbd82
garretthilberling <ghilberling@knights.ucf.edu>
2022-02-19 11:22:22 -0500

    created README template and began questions array. Want to provide the option for users to exclude license, tests, and FAQ sections

--- index.js
+++ index.js
@@ -4,7 +4,38 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'firstName',
+    message: 'What is first name?'
+},
+{
+  type: 'input',
+  name: 'lastName',
+  message: 'What is your last name?'
+},
+{
+  type: 'input',
+  name: 'github',
+  message: 'What is your github username?'
+},
+{
+  type: 'input',
+  name: 'linkedin',
+  message: 'Paste the link to your LinkedIn profile.'
+},
+{
+  type: 'input',
+  name: 'projectname',
+  message: 'What is the name of this project?'
+},
+{
+  type: 'input',
+  name: 'lastName',
+  message: 'Paste the link to the deployed URL of this application.'
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
